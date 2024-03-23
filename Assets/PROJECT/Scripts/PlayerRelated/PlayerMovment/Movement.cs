b4befdba691fd30bfd38016bb06e7b9b108d9a83
Avinash <avi.g751@gmail.com>
2024-03-23 12:15:23 +0400

    the slope physics is better than whatver the hel;l i did in dreadrun

--- Assets/PROJECT/Scripts/PlayerRelated/PlayerMovment/Movement.cs
+++ Assets/PROJECT/Scripts/PlayerRelated/PlayerMovment/Movement.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using CustomInspector;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float speed = 5;
-    [SerializeField] float tolerableSlopeAngle = 30;
+    [SelfFill][SerializeField] Rigidbody rb;
 
-    [SerializeField] bool isPlayerGrinding = false;
-    [SerializeField] Rigidbody rb;
+    [HorizontalLine("Ground Movement Info", 3, FixedColor.Gray)]
+    [SerializeField] float speed = 5;
+    [ReadOnly][SerializeField] bool isPlayerGrinding = false;
+
+    [HorizontalLine("Slope Movement Info", 3, FixedColor.Gray)]
+    [SerializeField] float tolerableSlopeAngle = 30;
+    [SerializeField] float downSlopeGravity = 30;
+    [SerializeField] float upSlopeSpeed = 1.25f;
+    [SerializeField] AnimationCurve slopeCurve;
+    [SerializeField] float slopeAlignDuration = 0.5f;
+    [ReadOnly][SerializeField] bool onSlope = false;
+    float slopeLerpFactor;
 
     float xInput;
     float zInput;
+
     Vector3 moveDirection;
+    Vector3 moveForce;
+
+    float time;
 
     public static Action<Vector3> OnMoveDirectionChanged;
 
@@ -29,11 +43,6 @@ public class Movement : MonoBehaviour
         Jump.GetExternalMomentum -= JumpMomentumAddon;
     }
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
     private void FixedUpdate()
     {
         if (isPlayerGrinding) return;
@@ -47,26 +56,56 @@ public class Movement : MonoBehaviour
         moveDirection = cameraForwardDirection * zInput + cameraRightDirection * xInput;
         moveDirection = moveDirection.normalized;
         OnMoveDirectionChanged?.Invoke(moveDirection);
-        Vector3 moveForce = new Vector3(moveDirection.x * speed, 0, moveDirection.z * speed);
-
-
+        moveForce = new Vector3(moveDirection.x * speed, 0, moveDirection.z * speed);
 
         if (moveDirection != Vector3.zero)
         {
             Quaternion forwardRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
-
             transform.rotation = Quaternion.Slerp(transform.rotation, forwardRotation, Time.deltaTime * 20);
-            RaycastHit hit;
-
-            if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.5f))
-            {
-                Quaternion targetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-                transform.rotation = targetRotation;
-                moveDirection = targetRotation * moveDirection;
-              
-            }
+            SlopePhysics();
         }
+
         rb.AddForce(moveForce, ForceMode.Acceleration);
+    }
+
+
+
+    void SlopePhysics()
+    {
+        RaycastHit hit;
+        float slopeAngle = 0;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 3f))
+        {
+            slopeAngle = Vector3.Angle(hit.normal, Vector3.up);
+            onSlope = slopeAngle < tolerableSlopeAngle && slopeAngle != 0;
+        }
+
+        if (!onSlope) { time = 0; return; }
+
+        moveDirection = Vector3.ProjectOnPlane(moveDirection, hit.normal).normalized;
+        moveDirection.y = 0;
+
+        if (rb.velocity.y < 0)
+        {
+            rb.AddForce(-hit.normal * downSlopeGravity, ForceMode.Acceleration);
+            moveForce = moveDirection * speed * upSlopeSpeed *1.5f;
+        }
+
+        moveForce = moveDirection * speed * upSlopeSpeed;
+        RotationSlopeAlignment(hit);
+    }
+
+    private void RotationSlopeAlignment(RaycastHit hit)
+    {
+        Quaternion targetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        if (time < slopeAlignDuration)
+        {
+            time += Time.deltaTime;
+            slopeLerpFactor = slopeCurve.Evaluate(time / slopeAlignDuration);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, slopeLerpFactor);
+        }
+        else { transform.rotation = targetRotation; }
     }
 
     public void SetIsPlayerGrinding(bool value)
